1cbd1bcd35962e8f2cf43cc52016556ff0fa52de
Y W <wangyaweiart@gmail.com>
2022-09-02 16:58:33 +0800

    Initial Commit

---
+++ Homework/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Homework
+//
+//  Created by Andy on 2022/9/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
